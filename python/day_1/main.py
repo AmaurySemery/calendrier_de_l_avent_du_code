85586cf983cb2f7e7fa5ad0027f8cad1e954aab3
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2023-12-02 14:42:18 +0100

    Correction de quelques coquilles pour le day_1

--- python/day_1/main.py
+++ python/day_1/main.py
@@ -1,6 +1,4 @@
-import os
 import csv
-import re
 
 file_path = 'python/day_1/input.csv'
 list_input = []
@@ -19,19 +17,16 @@ def extract_first_and_last_int(line):
                 last_int = int(char)
 
         if first_int is not None and last_int is not None:
-            if first_int == last_int:
-                print("Attention : La chaîne ne contient qu'un seul chiffre, il sera dupliqué.")
             concatenated_result = int(str(first_int) + str(last_int))
             convert_result = int(concatenated_result)
             return convert_result
         else:
             return None, None
 
-if os.path.exists(file_path):
-    with open(file_path, newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            first_and_last_int = extract_first_and_last_int(row)
-            list_input.append(first_and_last_int)
+with open(file_path, newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        first_and_last_int = extract_first_and_last_int(row)
+        list_input.append(first_and_last_int)
 
 print(sum(list_input))
